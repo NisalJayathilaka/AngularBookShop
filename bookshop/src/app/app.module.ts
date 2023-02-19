b1afc4e9b09013d087e0ac4d0beb519d8eca743f
NisalJayathilaka <nisal7075399@gmail.com>
2023-02-19 13:45:31 +0530

    add form validation

--- bookshop/src/app/app.module.ts
+++ bookshop/src/app/app.module.ts
@@ -3,12 +3,13 @@ import { FormsModule } from "@angular/forms";
 import { BrowserModule } from "@angular/platform-browser";
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
+import { AuthModule } from "./auth/auth.module";
 import { BookModule } from "./book/book.module";
 import { CartComponent } from './cart/cart.component';
 
 
 @NgModule({
-    imports:[BrowserModule,FormsModule,BookModule,AppRoutingModule],
+    imports:[BrowserModule,BookModule,AppRoutingModule,AuthModule],
     declarations:[AppComponent, CartComponent],
     bootstrap:[AppComponent]
 })
--- bookshop/src/app/auth/register/register.component.ts
+++ bookshop/src/app/auth/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RegisterForm } from 'src/app/interface/Auth';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.css']
 })
 export class RegisterComponent implements OnInit {
-
+  form: RegisterForm={
+    email : '',
+    password : '',
+    confirmPassword:''
+  }
   constructor() { }
 
   ngOnInit(): void {
   }
-
+  submit(){
+    console.log(this.form);
+    
+  }
 }
